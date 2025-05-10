425a3e05de1212ab312ca55966a2b56783e30696
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-11 01:57:45 +0300

    firstcommit

---
+++ eureka-service/src/test/java/com/kitaplikmicroservices/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitaplikmicroservices.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
