54ab7832bad3f5d4e7709a4d2d3d9072da393cc2
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-15 23:33:27 +0300

    RetreiveMessageErrorDecoder

--- library-service/src/main/java/com/kitaplikmicroservices/library_service/LibraryServiceApplication.java
+++ library-service/src/main/java/com/kitaplikmicroservices/library_service/LibraryServiceApplication.java
@@ -1,8 +1,11 @@
 package com.kitaplikmicroservices.library_service;
 
+import com.kitaplikmicroservices.library_service.client.RetreiveMessageErrorDecoder;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +15,9 @@ public class LibraryServiceApplication {
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
 
+
+	@Bean
+	public ErrorDecoder errorDecoder(){
+		return new RetreiveMessageErrorDecoder();
+	}
 }
