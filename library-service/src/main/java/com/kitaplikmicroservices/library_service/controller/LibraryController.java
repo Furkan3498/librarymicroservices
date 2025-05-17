a49b3934b55e82ec1dd1fa2427beaeeb5eecbd19
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-17 23:08:22 +0300

    Fault Tolerance -circuitbreaker-resilience4j

--- library-service/src/main/java/com/kitaplikmicroservices/library_service/controller/LibraryController.java
+++ library-service/src/main/java/com/kitaplikmicroservices/library_service/controller/LibraryController.java
@@ -3,6 +3,8 @@ package com.kitaplikmicroservices.library_service.controller;
 import com.kitaplikmicroservices.library_service.dto.AddBookRequest;
 import com.kitaplikmicroservices.library_service.dto.LibraryDto;
 import com.kitaplikmicroservices.library_service.service.LibraryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/library")
 public class LibraryController {
 
-
+    Logger logger = LoggerFactory.getLogger(LibraryController.class);
     private final LibraryService libraryService;
     private final Environment environment;
 
